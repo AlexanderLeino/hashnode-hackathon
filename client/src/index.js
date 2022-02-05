7090140a65e4bfcca7cf33764c7b5efa47372035
Alexander Leino <Alexander.Leino@yahoo.com>
2022-02-04 23:12:12 -0500

    added more seed data. added apolloclient to app.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'
+import {
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProivder,
+} from '@apollo/client';
+
+
+const client = new ApolloClient({
+  uri: 'graphql',
+  cache: new InMemoryCache()
+})
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProivder client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProivder>,
   document.getElementById('root')
 );
