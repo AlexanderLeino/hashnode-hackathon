e76a50a842cd3a6a39e55c813f273e28ded89469
Alex Cortez <acim650@gmail.com>
2022-02-09 13:15:49 -0800

    stashBranch

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './HomePage.css'
-import SidebarCategories from '../components/SidebarCategories'
 import SearchBar from '../components/SearchBar'
 import HeroSection from '../components/HeroSection'
 import DashboardMenu from '../components/DashboardMenu'
@@ -12,14 +11,13 @@ function HomePage() {
     <div>
         <SearchBar />
           <div className='wrapper'>
-            <div className='box-one'>
+            <div>
               <DashboardMenu />
             </div>
             <div className='box-two'>
-              <SidebarCategories />
+              <HeroSection />
             </div>
           </div>
-        <HeroSection />
         <AboutSection />
         <Footer />
     </div>
