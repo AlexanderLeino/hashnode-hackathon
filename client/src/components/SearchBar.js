3e2a0849b340ca11ea8384ed031fbbd4133bc6d3
Alex Cortez <acim650@gmail.com>
2022-02-10 15:25:37 -0800

    newBranch

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import Box from '@mui/material/Box';
 import './SearchBar.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 
 function SearchBar() {
   return (
-    <div>
-        <Box className='searchBar'>
-            <input className='innerSearchBar'>
-            </input>
-        </Box>
+    <div className='searchbar-container'>
+        <Box
+      sx={{
+        width: 500,
+        maxWidth: '100%',
+      }}
+    >
+      <TextField fullWidth label="fullWidth" id="fullWidth" />
+    </Box>
+  );
     </div>
   )
 }
-
 
 export default SearchBar;
--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './HomePage.css'
-import SidebarCategories from '../components/SidebarCategories'
 import SearchBar from '../components/SearchBar'
 import HeroSection from '../components/HeroSection'
 import DashboardMenu from '../components/DashboardMenu'
@@ -15,9 +14,6 @@ function HomePage() {
           <div className='wrapper'>
             <div className='box-one'>
               <DashboardMenu />
-            </div>
-            <div className='box-two'>
-              <SidebarCategories />
             </div>
           </div>
         <HeroSection />
