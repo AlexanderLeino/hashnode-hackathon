55c471a5dabbb3a60b4179f50e572acad0e5f870
Alexander Leino <Alexander.Leino@yahoo.com>
2022-02-10 19:32:49 -0500

    added step component to initialSignUp Page

--- client/src/pages/InitialSignUpPage.js
+++ client/src/pages/InitialSignUpPage.js
@@ -4,9 +4,22 @@ import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import humanCoding from '../images/human-coding.png'
 import Button from '@mui/material/Button'
-import SignUpStepper from '../components/SignUpStepper'
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
 
 function InitialSignUpPage() {
+    const steps = ['Who Are You?', 'Select Preferred Technologies', 'Account Creation Complete'];
+    const [activeStep, setActiveStep] = React.useState(0);
+
+
+    const handleNext = () => {
+     setActiveStep((prevActiveStep) => prevActiveStep + 1);
+   };
+ 
+   const handleBack = () => {
+     setActiveStep((prevActiveStep) => prevActiveStep - 1);
+   };
     // const [userName, setUserName] = useState('')
     // const [email, setEmail] = useState('')
     // const [password, setPassword] = useState('')
@@ -39,7 +52,24 @@ function InitialSignUpPage() {
     <Container maxWidth style={{background:'#393da9d9', paddingTop:'2rem', paddingBottom:'2rem'}}>
         
         <Container maxWidth='sm'>
-            <SignUpStepper />
+        <Box sx={{ width: '100%' }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label) => {
+          const stepProps = {};
+          const labelProps = {};
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+
+        <>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+          </Box>
+        </>
+    </Box>
         </Container>
 
     <Container maxWidth='sm' style={{padding:'1.25rem', borderRadius:'25px', background:'white', boxShadow:'5px 10px 18px  black', border: '1px solid black'}}>
@@ -55,8 +85,8 @@ function InitialSignUpPage() {
         </form>
     </Container>
         <Box style={{display:'flex', justifyContent:'flex-end'}}>
-            <Button variant='contained'>Next</Button>
-            <Button variant='contained'>Next</Button>
+            <Button variant='contained' onClick={handleBack} disabled={activeStep === 0}>Back</Button>
+            <Button variant='contained' onClick={handleNext}>Next</Button>
         </Box>
     </Container>
 
